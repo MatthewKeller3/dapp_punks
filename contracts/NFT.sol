0debc39f1ea7fd74bcb92357f655543065e344d9
Kelchain <MatthewJKeller@gmail.com>
2025-02-11 22:29:46 -0700

    Multiple NFT's testing (success)

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,3 +1,41 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "./ERC721Enumerable.sol";
+import "./Ownable.sol";
+
+contract NFT is ERC721Enumerable, Ownable { 
+    uint256 public cost; 
+    uint256 public maxSupply;
+    uint256 public allowMintingOn;
+    string public baseURI;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _cost,
+        uint256 _maxSupply,
+        uint256 _allowMintingOn,
+        string  memory _baseURI
+    ) ERC721(_name, _symbol) {
+        cost = _cost;
+        maxSupply = _maxSupply;
+        allowMintingOn = _allowMintingOn;
+        baseURI = _baseURI;
+  }
+
+    function mint(uint256 _mintAmount) public payable {
+       
+        uint256 supply = totalSupply();
+
+        require(msg.value >= cost * _mintAmount);
+
+        for(uint256 i = 1; i <= _mintAmount; i++) {
+            _safeMint(msg.sender, supply + i);
+
+    }
+    
+}
+   
+
+}
